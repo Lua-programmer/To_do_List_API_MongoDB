3798a98421a220183c280d4bca5d371657a881d9
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-10-16 22:56:41 -0300

    routes

---
+++ routes/tasks.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const TasksController = require('../controllers/tasks.controller');
+
+const router = express.Router();
+const tasksController = new TasksController();
+
+router.get('/', tasksController.getTasks);
+
+router.get('/:id', tasksController.getTasksById);
+
+router.post('/add', tasksController.createTask);
+
+router.put('/:id', tasksController.updateTask);
+
+router.delete('/:id', tasksController.deleteTask);
+
+module.exports = router;
