90382528724aa0ac2c64c44bd9f2dd475522856b
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-10-16 23:07:41 -0300

    criando const newLocal

--- models/task.js
+++ models/task.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const TaskModel = new.mongoose.Schema({
+const newLocal = { type: Date, default: Date.now };
+const TaskModel = new mongoose.Schema({
     title: { type: String, required: true }, //titulo
     description: { type: String, required: true }, //descricao
     priority: { type: String, required: true }, //prioridade(alta, media e baixa)
     status: { type: String, required: true }, //fazer, fazendo, feito
     deadline: { type: String, required: true }, //prazo
-    creationDate: { type: Date, default: Date.now}
+    creationDate: newLocal
 })
 
 const Task = mongoose.model("tasks", TaskModel);
